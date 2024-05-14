f631c1528f33221e908df65a2316226c26266656
Abhishek Deshpande <95285894+AbhishekDeshpande2002@users.noreply.github.com>
2024-05-15 00:52:22 +0530

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,14 @@
 plugins {
     alias(libs.plugins.androidApplication)
+    id("com.google.gms.google-services")
 }
 
 android {
-    namespace = "com.example.camera1"
+    namespace = "com.example.camera"
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.example.camera1"
+        applicationId = "com.example.camera"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
@@ -40,11 +41,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.ext.junit)
     androidTestImplementation(libs.espresso.core)
-    implementation("androidx.camera:camera-core:1.3.2")
-    implementation("androidx.camera:camera-camera2:1.3.2")
-    implementation("androidx.camera:camera-lifecycle:1.3.3")
-    implementation("androidx.camera:camera-view:1.3.2")
-
-
-
+    implementation ("com.google.firebase:firebase-storage:20.3.0")
 }
