507d2164bc68d6e03240b84aa2ccc5bb86f8a3b4
Abhishek Deshpande <95285894+AbhishekDeshpande2002@users.noreply.github.com>
2024-05-15 00:54:47 +0530

    Update MainActivity.java

--- MainActivity.java
+++ MainActivity.java
@@ -1,210 +1,121 @@
-package com.example.camera1;
+package com.example.camera;
 
-import static android.os.Environment.getExternalStoragePublicDirectory;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import androidx.activity.result.ActivityResult;
 import androidx.activity.result.ActivityResultCallback;
 import androidx.activity.result.ActivityResultLauncher;
 import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.camera.core.AspectRatio;
-import androidx.camera.core.Camera;
-import androidx.camera.core.CameraSelector;
-import androidx.camera.core.ImageCapture;
-import androidx.camera.core.ImageCaptureException;
-import androidx.camera.core.Preview;
-import androidx.camera.lifecycle.ProcessCameraProvider;
-import androidx.camera.view.PreviewView;
-import androidx.core.content.ContextCompat;
+import androidx.appcompat.view.menu.MenuBuilder;
 
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
-import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
-import android.graphics.Point;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.Environment;
-import android.view.View;
-import android.widget.FrameLayout;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Toast;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.button.MaterialButton;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
 
-import com.google.common.util.concurrent.ListenableFuture;
-
-import org.w3c.dom.Text;
-
-import java.io.File;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Executors;
+import java.text.MessageFormat;
+import java.util.ArrayList;
+import java.util.Objects;
+import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
-    Uri imageUri;
-    ImageButton capture, toggleFlash, flipCamera;
-    Text watermark;
-
-    private PreviewView previewView;
-    int cameraFacing = CameraSelector.LENS_FACING_BACK;
-    /* public static Bitmap mark(Bitmap src, String watermark, Point location, Color color, int alpha, int size, boolean underline ){
-        int w = src.getWidth();
-        int h = src.getHeight();
-        Bitmap result = Bitmap.createBitmap(w,h,src.getConfig());
-
-        Canvas canvas = new Canvas(result);
-        canvas.drawBitmap(src,0,0,null);
-
-        Paint paint = new Paint();
-        paint.setColor(color);
-        paint.setAlpha(alpha);
-        paint.setTextSize(size);
-        paint.setAntiAlias(true);
-        paint.setUnderlineText(underline);
-        canvas.drawText(watermark,location.x,location.y,paint);
-
-        return result;
-    }*/
-    private final ActivityResultLauncher<String> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+    private ArrayList<Uri> arrayList;
+    private final ActivityResultLauncher<Intent> activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
         @Override
-        public void onActivityResult(Boolean result) {
-            if (result) {
-                startCamera(cameraFacing);
+        public void onActivityResult(ActivityResult o) {
+            if (o.getResultCode() == RESULT_OK) {
+                arrayList.clear();
+                if (o.getData() != null && o.getData().getClipData() != null) {
+                    int count = o.getData().getClipData().getItemCount();
+                    for (int i = 0; i < count; i++) {
+                        Uri imageUri = o.getData().getClipData().getItemAt(i).getUri();
+                        arrayList.add(imageUri);
+                    }
+                    if (arrayList.size() >= 1) {
+                        upload.setEnabled(true);
+                        textView.setText(MessageFormat.format("{0} Images selected.", arrayList.size()));
+                    }
+                }
             }
         }
     });
+    TextView textView;
+    MaterialButton select, upload;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        previewView = findViewById(R.id.cameraPreview);
-        capture = findViewById(R.id.capture);
-        toggleFlash = findViewById(R.id.toggleFlash);
-        flipCamera = findViewById(R.id.flipCamera);
+        FirebaseApp.initializeApp(MainActivity.this);
 
-        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
-            activityResultLauncher.launch(Manifest.permission.CAMERA);
-        } else {
-            startCamera(cameraFacing);
-        }
+        arrayList = new ArrayList<>();
 
-        flipCamera.setOnClickListener(new View.OnClickListener() {
+        select = findViewById(R.id.selectImages);
+        upload = findViewById(R.id.uploadImages);
+        textView = findViewById(R.id.selectedTv);
+
+        select.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (cameraFacing == CameraSelector.LENS_FACING_BACK) {
-                    cameraFacing = CameraSelector.LENS_FACING_FRONT;
-                } else {
-                    cameraFacing = CameraSelector.LENS_FACING_BACK;
-                }
-                startCamera(cameraFacing);
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                activityResultLauncher.launch(intent);
+            }
+        });
+
+        upload.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                upload.setText("Uploading images...");
+                upload.setEnabled(false);
+                uploadImages(new ArrayList<>());
             }
         });
     }
 
-    public void startCamera(int cameraFacing) {
-        int aspectRatio = aspectRatio(previewView.getWidth(), previewView.getHeight());
-        ListenableFuture<ProcessCameraProvider> listenableFuture = ProcessCameraProvider.getInstance(this);
-
-        listenableFuture.addListener(() -> {
-            try {
-                ProcessCameraProvider cameraProvider = (ProcessCameraProvider) listenableFuture.get();
-
-                Preview preview = new Preview.Builder().setTargetAspectRatio(aspectRatio).build();
-
-                ImageCapture imageCapture = new ImageCapture.Builder().setCaptureMode(ImageCapture.CAPTURE_MODE_MINIMIZE_LATENCY)
-                        .setTargetRotation(getWindowManager().getDefaultDisplay().getRotation()).build();
-
-                CameraSelector cameraSelector = new CameraSelector.Builder()
-                        .requireLensFacing(cameraFacing).build();
-
-                cameraProvider.unbindAll();
-
-                Camera camera = cameraProvider.bindToLifecycle(this, cameraSelector, preview, imageCapture);
-
-                capture.setOnClickListener(new View.OnClickListener() {
+    private void uploadImages(ArrayList<String> imageUrlList) {
+        StorageReference storageReference = FirebaseStorage.getInstance().getReference().child("images").child(UUID.randomUUID().toString());
+        Uri uri = arrayList.get(imageUrlList.size());
+        storageReference.putFile(uri).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+            @Override
+            public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                storageReference.getDownloadUrl().addOnCompleteListener(new OnCompleteListener<Uri>() {
                     @Override
-                    public void onClick(View view) {
-                        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
-                            activityResultLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+                    public void onComplete(@NonNull Task<Uri> task) {
+                        String url = task.getResult().toString();
+                        imageUrlList.add(url);
+
+                        if(arrayList.size() == imageUrlList.size()) {
+                            //Uploaded images url is stored in imagesUrl ArrayList.
+                            Toast.makeText(MainActivity.this, "Images uploaded successfully!", Toast.LENGTH_SHORT).show();
+                            upload.setText("Upload images");
+                            upload.setEnabled(true);
+                        }else {
+                            uploadImages(imageUrlList);
                         }
-                        takePicture(imageCapture);
                     }
-                });
-
-                toggleFlash.setOnClickListener(new View.OnClickListener() {
+                }).addOnFailureListener(new OnFailureListener() {
                     @Override
-                    public void onClick(View view) {
-                        setFlashIcon(camera);
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(MainActivity.this,"Failed to upload image", Toast.LENGTH_SHORT).show();
                     }
                 });
-
-                preview.setSurfaceProvider(previewView.getSurfaceProvider());
-            } catch (ExecutionException | InterruptedException e) {
-                e.printStackTrace();
-            }
-        }, ContextCompat.getMainExecutor(this));
-    }
-
-    public void takePicture(ImageCapture imageCapture) {
-        //final
-        File storageDir = new File(getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES),System.currentTimeMillis() + ".jpg");
-        //Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES);
-        if (!storageDir.exists()){
-            storageDir.mkdir();
-        }
-        ImageCapture.OutputFileOptions outputFileOptions = new ImageCapture.OutputFileOptions.Builder(storageDir).build();
-        imageCapture.takePicture(outputFileOptions, Executors.newCachedThreadPool(), new ImageCapture.OnImageSavedCallback() {
-
-            @Override
-            public void onImageSaved(@NonNull ImageCapture.OutputFileResults outputFileResults) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "Image saved at: " + storageDir.getPath(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-                startCamera(cameraFacing);
-            }
-
-            @Override
-            public void onError(@NonNull ImageCaptureException exception) {
-                runOnUiThread(new Runnable() {
-                    @Override
-                    public void run() {
-                        Toast.makeText(MainActivity.this, "Failed to save: " + exception.getMessage(), Toast.LENGTH_SHORT).show();
-                    }
-                });
-                startCamera(cameraFacing);
             }
         });
-    }
-
-    private void setFlashIcon(Camera camera) {
-        if (camera.getCameraInfo().hasFlashUnit()) {
-            if (camera.getCameraInfo().getTorchState().getValue() == 0) {
-                camera.getCameraControl().enableTorch(true);
-                toggleFlash.setImageResource(R.drawable.baseline_flash_off_24);
-            } else {
-                camera.getCameraControl().enableTorch(false);
-                toggleFlash.setImageResource(R.drawable.baseline_flash_on_24);
-            }
-        } else {
-            runOnUiThread(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(MainActivity.this, "Flash is not available currently", Toast.LENGTH_SHORT).show();
-                }
-            });
-        }
-    }
-
-    private int aspectRatio(int width, int height) {
-        double previewRatio = (double) Math.max(width, height) / Math.min(width, height);
-        if (Math.abs(previewRatio - 4.0 / 3.0) <= Math.abs(previewRatio - 16.0 / 9.0)) {
-            return AspectRatio.RATIO_4_3;
-        }
-        return AspectRatio.RATIO_16_9;
     }
 }
